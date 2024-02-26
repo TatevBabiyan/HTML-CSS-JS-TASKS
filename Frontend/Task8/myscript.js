8b3a8ab27cddb582423371e60af7b63e7fde7505
TatevBabiyan <116426513+TatevBabiyan@users.noreply.github.com>
2024-02-26 12:08:35 +0100

    Add files via upload

---
+++ Frontend/Task8/myscript.js
@@ -0,0 +1,51 @@
+const formId = "save-later-form";
+const url = location.href;
+const formIdentifier = `${url} ${formId}`;
+const saveButton = document.querySelector("#save");
+const alertBox = document.querySelector(".alert");
+let form = document.querySelector(`#${formId}`);
+let formElements = form.elements;
+
+
+const getFormData = () => {
+  let data = { [formIdentifier]: {} };
+  for (const element of formElements) {
+    if (element.name.length > 0) {
+      data[formIdentifier][element.name] = element.value;
+    }
+  }
+  return data;
+};
+
+saveButton.onclick = event => {
+  event.preventDefault();
+  data = getFormData();
+  localStorage.setItem(formIdentifier, JSON.stringify(data[formIdentifier]));
+  const message = "Form has been saved!";
+  displayAlert(message);
+};
+
+
+const displayAlert = message => {
+  alertBox.innerText = message;
+  alertBox.style.display = "block";
+  setTimeout(function() {
+    alertBox.style.display = "none";
+  }, 1000);
+};
+
+
+const populateForm = () => {
+  if (localStorage.key(formIdentifier)) {
+    const savedData = JSON.parse(localStorage.getItem(formIdentifier));
+    for (const element of formElements) {
+      if (element.name in savedData) {
+        element.value = savedData[element.name];
+      }
+    }
+    const message = "Form has been refilled with saved data!";
+    displayAlert(message);
+  }
+};
+
+document.onload = populateForm();
